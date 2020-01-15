f28c8ddc1a3b97d588979d4f6887c8701e0919e5
Airlangga, Mikael <mikael.airlangga@gmail.com>
2020-01-15 22:33:06 +0800

    8-113. More Express API Setup

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ const {
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const redis = require("redis");
 
 // new express application
 const app = express();
@@ -34,3 +35,49 @@ pgClient.on("error", () => console.log("Lost PG connection..."));
 pgClient
     .query("CREATE TABLE IF NOT EXISTS values(number INT)")
     .catch(err => console.log(err));
+
+// redis config
+const redisClient = redis.createClient({
+    host: redisHost,
+    port: redisPort,
+    retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
+
+// express route handlers
+app.get("/", (req, res) => {
+    res.send("Hi!");
+});
+
+app.get("/values/all", async (req, res) => {
+    const values = await pgClient.query("SELECT * FROM values");
+    res.send(values.rows);
+});
+
+app.get("/values/current", async (req, res) => {
+    redisClient.hgetall("values", (err, values) => {
+        res.send(values);
+    });
+});
+
+app.post("/values", async (req, res) => {
+    const index = req.body.index;
+
+    if (parseInt(index) > 40) {
+        return res.status(422).send("Index too high");
+    }
+
+    // post to redis
+    redisClient.hset("values", index, "Nothing yet!");
+    redisPublisher.publish("insert", index);
+
+    // post to postgres
+    pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
+
+    // send back the response
+    res.send({ working: true });
+});
+
+app.listen(5000, () => {
+    console.log("App is listening on porrt 5000");
+});
